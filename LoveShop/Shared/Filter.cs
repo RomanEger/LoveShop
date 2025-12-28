92500c3328e9b9e02d36f881c8b8598836e93a20
RomanEger <egerromaan@gmail.com>
2025-12-28 14:54:25 +0600

    create filter record

---
+++ LoveShop/Shared/Filter.cs
@@ -0,0 +1,10 @@
+using System.Linq.Expressions;
+
+namespace LoveShop.Shared
+{
+    public sealed record Filter<T>(
+        int PageNumber,
+        int PageSize,
+        Expression<Func<T, bool>>? Condition = null
+    );
+}
