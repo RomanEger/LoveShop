2775b3df22b40d54afee6e18224aec7a955154f0
RomanEger <egerromaan@gmail.com>
2025-12-30 23:03:19 +0600

    add migration with row version

---
+++ LoveShop/Migrations/20251230170225_RowVersion.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoveShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class RowVersion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "suppliers",
+                type: "bytea",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "products_in_cart",
+                type: "bytea",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "products",
+                type: "bytea",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "product_categories",
+                type: "bytea",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "orders",
+                type: "bytea",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "order_items",
+                type: "bytea",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "customers",
+                type: "bytea",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "categories",
+                type: "bytea",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "carts",
+                type: "bytea",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "products_in_cart");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "products");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "product_categories");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "orders");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "order_items");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "customers");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "categories");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "carts");
+        }
+    }
+}
