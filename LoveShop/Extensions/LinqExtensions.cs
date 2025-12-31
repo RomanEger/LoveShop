c0e4f4c14f4242fbf8ad5addabca2336e6ddbb05
RomanEger <egerromaan@gmail.com>
2025-12-31 21:59:38 +0600

    add LINQ paginate extension method

---
+++ LoveShop/Extensions/LinqExtensions.cs
@@ -0,0 +1,17 @@
+﻿using LoveShop.Shared;
+
+namespace LoveShop.Extensions
+{
+	public static class LinqExtensions
+	{
+		public static IQueryable<TEntity> Paginate<TEntity>(
+			this IQueryable<TEntity> source,
+			PaginatedFilter<TEntity> paginatedFilter
+		)
+		{
+			return source
+				.Skip(paginatedFilter.PageNumber * paginatedFilter.PageSize)
+				.Take(paginatedFilter.PageSize);
+		}
+	}
+}
