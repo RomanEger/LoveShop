bf734e8f0db28f0ee58bda8eaa46dbb8e4aa9572
RomanEger <egerromaan@gmail.com>
2026-01-10 15:29:09 +0600

    create product update DTO

---
+++ LoveShop/DTOs/Product/ProductUpdateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace LoveShop.DTOs.Product
+{
+	public sealed record ProductUpdateDTO(
+		Guid Id,
+		string Name,
+		string? Description,
+		decimal Price,
+		ICollection<Guid> CategoriesIds,
+		byte[] RowVersion);
+}
